e32fc9625eb84f9d6363d09840d0e8c3be53b82f
champuser <aidan.pasek@mymail.champlain.edu>
2024-09-08 19:08:07 -0400

    Uploaded code for Process Management Script 4

---
+++ processmanagement_code4.ps1
@@ -0,0 +1,11 @@
+﻿# Checks for one of two instances of this script:
+# If an instance of this script is running already, stop it.
+$pathToexecute = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$startPage = "https://www.champlain.edu"
+$data = "--user-data-dir=c:\temp"
+if ($pathToexecute -eq $pathToexecute){
+    Get-Process -Name chrome | Stop-Process}
+# If an instance of this script is not running already, it starts Google Chrome web
+# browser and directs to Champlain.edu.
+if ($pathToexecute -notcontains "chrome.exe"){
+    Start-Process -FilePath $pathToexecute $startPage}
