690d3be1c4b55dd52c21adf76a613190f9a4c110
champuser <aidan.pasek@mymail.champlain.edu>
2024-09-22 15:21:23 -0400

    Uploaded code for week 4 assignments

---
+++ week4/Apache-Logs1.ps1
@@ -0,0 +1,32 @@
+﻿# Write a function that performs the following:
+# 1. Parses Apache logs word by word into a PSCustomObject.
+# 2. Filters the IP property of the return object for the 10.* network.
+
+# Create the function
+function ApacheLogs1(){
+
+# Get content from the logs
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+
+# Create the array
+$tableRecords = @()
+for($i=0; $i -lt $logsNotformatted.Count; $i++){
+
+# Split a string into words
+$words = $logsNotformatted[$i].Split(" ");
+
+# Define the PSCustomObject
+ $tableRecords += [PSCustomObject]@{ "IP" = $words[0]; `
+                                     "Time" = $words[3].Trim('['); `
+                                     "Method" = $words[5].Trim('"'); `
+                                     "Page" = $words[6]; `
+                                     "Protocol" = $words[7]; `
+                                     "Response" = $words[8]; `
+                                     "Referrer" = $words[10]; `
+                                     "Client" = $words[11..($words.Length - 1)]; }
+}
+# Return the results that include IP properties for the 10.* network
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
--- week4/main.ps1
+++ week4/main.ps1
@@ -1,12 +1,20 @@
 ﻿# Calls the function "ApacheLogs" from the file "Apache-Logs.ps1" and counts the IP addresses
-. (Join-Path $PSScriptRoot '.\Apache-Logs.ps1')
+#. (Join-Path $PSScriptRoot '.\Apache-Logs.ps1')
+#clear
+
+## Define the user inputs
+#$pageVisited = Read-Host "Enter the page visited or referred from"
+#$codeHTTP = Read-Host "Enter the HTTP code returned"
+#$nameofbrowser = Read-Host "Enter the name of the web browser"
+
+## Call the function with the parameters
+#$ipsamount = ApacheLogs $pageVisited $codeHTTP $nameofbrowser
+#$ipsamount
+#____________________________________________________________________________________________#
+
+# Calls the function "ApacheLogs1" from the file "Apache-Logs1.ps1" with the appropriate tasks
+. (Join-Path $PSScriptRoot '.\Apache-Logs1.ps1')
 clear
 
-# Define the user inputs
-$pageVisited = Read-Host "Enter the page visited or referred from"
-$codeHTTP = Read-Host "Enter the HTTP code returned"
-$nameofbrowser = Read-Host "Enter the name of the web browser"
-
-# Call the function with the parameters
-$ipsamount = ApacheLogs $pageVisited $codeHTTP $nameofbrowser
-$ipsamount
+$parsedIPs = ApacheLogs1
+$parsedIPs | Format-Table -AutoSize -Wrap
